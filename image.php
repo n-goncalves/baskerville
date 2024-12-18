n-goncalves/baskerville

commit 0711af1df7ba70fa94e4d084398f4d7687db8fca

V 2.0.0

image.php
@@ -1,65 +1,73 @@
 <?php get_header(); ?>
 
-<div class="wrapper section medium-padding">
+<div class="wrapper section medium-padding" id="site-content">
 
 	<div class="section-inner">
 
 		<div class="content fleft">
 												        
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php 
 			
-				<div id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
-														
-					<div class="featured-media">
+			if ( have_posts() ) : 
+			
+				while ( have_posts() ) : 
+				
+					the_post(); 
 					
-						<?php $image_array = wp_get_attachment_image_src( $post->ID, 'full', false ); ?>
-					
-						<a href="<?php echo esc_url( $image_array[0] ); ?>" title="<?php the_title_attribute(); ?>" rel="attachment">
-							<?php echo wp_get_attachment_image( $post->ID, 'post-image' ); ?>
-						</a>
-					
-					</div><!-- .featured-media -->
-					
-					<div class="post-header">
-					
-						<h2 class="post-title"><?php echo basename( get_attached_file( $post->ID ) ); ?></h2>
-					
-					</div><!-- .post-header -->
-																	
-					<div class="post-meta-container">
-					
-						<div class="post-author">
+					?>
+				
+					<div id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
+															
+						<div class="featured-media">
 						
-							<div class="post-author-content">
+							<?php $image_url = wp_get_attachment_image_url( $post->ID, 'full' ); ?>
+						
+							<a href="<?php echo esc_url( $image_url ); ?>" rel="attachment">
+								<?php echo wp_get_attachment_image( $post->ID, 'post-image' ); ?>
+							</a>
+						
+						</div><!-- .featured-media -->
+						
+						<div class="post-header">
+						
+							<h1 class="post-title"><?php echo basename( get_attached_file( $post->ID ) ); ?></h1>
+						
+						</div><!-- .post-header -->
+																		
+						<div class="post-meta-container">
+						
+							<div class="post-author">
 							
-								<h4><?php _e( 'About the attachment', 'baskerville' ); ?></h4>
+								<div class="post-author-content">
 								
-								<p><?php the_excerpt(); ?></p>
+									<h4><?php _e( 'About the attachment', 'baskerville' ); ?></h4>
+									
+									<p><?php the_excerpt(); ?></p>
+								
+								</div><!-- .post-author-content -->
 							
-							</div><!-- .post-author-content -->
+							</div><!-- .post-author -->
+							
+							<div class="post-meta">
+							
+								<p class="post-date"><?php the_time( get_option( 'date_format' ) ); ?></p>
+								<p class="image-resolution"><?php echo $image_array[1] . ' <span style="text-transform:lowercase;">x</span>' . $image_array[2] . ' px'; ?></p>
+							
+							</div><!-- .post-meta -->
+							
+							<div class="clear"></div>
 						
-						</div><!-- .post-author -->
+						</div><!-- .post-meta-container -->
 						
-						<div class="post-meta">
+						<?php 
 						
-							<p class="post-date"><?php the_time( get_option( 'date_format' ) ); ?></p>
-							<p class="image-resolution"><?php echo $image_array[1] . ' <span style="text-transform:lowercase;">x</span>' . $image_array[2] . ' px'; ?></p>
-						
-						</div><!-- .post-meta -->
-						
-						<div class="clear"></div>
+						comments_template( '', true );
 					
-					</div><!-- .post-meta-container -->
-					
-					<?php comments_template( '', true );
-				
-				endwhile;
+					endwhile;
 			
-				else: ?>
-			
-					<p><?php _e( "We couldn't find any posts that matched your query. Please try again.", "baskerville" ); ?></p>
+				endif;
 				
-				<?php endif; ?>    
+				?>
 					
 			</div><!-- .post -->
 				

commit 21d5fb22280a9927fc6445c16e56a15c591fadf8

Fix Undefined variable $image_array in image.php
This bug has been introduced in commit 0711af1 and is also referenced here:

https://wordpress.org/support/topic/undefined-variable-image_array/

image.php
@@ -20,9 +20,9 @@
 															
 						<div class="featured-media">
 						
-							<?php $image_url = wp_get_attachment_image_url( $post->ID, 'full' ); ?>
+							<?php $image_array = wp_get_attachment_image_src( $post->ID, 'full' ); ?>
 						
-							<a href="<?php echo esc_url( $image_url ); ?>" rel="attachment">
+							<a href="<?php echo esc_url( $image_array[0] ); ?>" rel="attachment">
 								<?php echo wp_get_attachment_image( $post->ID, 'post-image' ); ?>
 							</a>
 						